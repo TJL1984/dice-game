df54ecd9242825767725d60940e5e18538600f88
TJ Liwanag <timothyjliwanag@gmail.com>
2016-10-24 16:17:13 -0400

    made a second die to randomize with the first

---
+++ js/dicegame.js
@@ -0,0 +1,27 @@
+(function diceGame() {
+
+    // Use document.querySelector('...') to find HTML elements
+    // The argument into the function is a CSS selector!
+    // Any HTML element has an `innerText` property that you can
+    // access (or change) for the contents of that element.
+
+    document.querySelector('button').addEventListener('click', function buttonClicked() {
+
+        var dieOne = Math.floor(Math.random()*6 + 1);
+        console.log(dieOne);
+        var dieTwo = Math.floor(Math.random()*6 + 1);
+        console.log(dieTwo);
+
+        var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
+        var dieNumTwo = document.querySelector('.dice aside:first-child').innerText;
+
+        
+
+
+
+
+
+    });
+
+
+})();
